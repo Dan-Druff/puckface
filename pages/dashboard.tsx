39ef68f96f53ed0f3e1af2bcf767618508841466
DanHam2012 <hamtron2000@gmail.com>
2022-04-03 17:30:06 -0400

    more anys

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,12 +2,12 @@ import type { NextPage } from 'next'
 import styles from '../styles/All.module.css'
 
 
-const Renderer = (Wrapped) => {
-    return function New(props){
+const Renderer = (Wrapped:any) => {
+    return function New(props:any){
         return <Wrapped {...props}/>
     }
 }
-const Child = (props) => {
+const Child = (props:any) => {
     return (
         <h1>Hello from child {props.name}</h1>
     )
